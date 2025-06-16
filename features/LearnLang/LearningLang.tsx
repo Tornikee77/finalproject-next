f183844ab2a09ec2542a3e77fade3539d67a6707
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-06-17 00:42:30 +0400

    feature/learninglangPart2

--- features/LearnLang/LearningLang.tsx
+++ features/LearnLang/LearningLang.tsx
@@ -11,14 +11,41 @@ const LearningLang = () => {
         <p className="mt-3 mb-5 text-[var(--color-paragraph)]">
           Make learning words more fun with mini-games
         </p>
-        <div>
-          <div className="pinkShoesCont w-[114px] h-[112px] rounded-2xl bg-[aqua] ">
-            <div>
+        <div className="flex gap-[30px] mx-auto justify-center  mt-[50px] ">
+          <div className="pinkShoesCont w-[114px] h-[112px] rounded-2xl bg-[var(--color-PinkshoeCont)] ">
+            <div className="relative">
               <Image
                 className="pt-[80px] mx-auto"
-                src="/Sprint →.png"
+                src="/Sprint →.svg"
                 width={53}
                 height={14}
+                alt="womanpng"
+              />
+
+              <Image
+                className="absolute bottom-8"
+                src="/pinkShoes.svg"
+                width={131}
+                height={106}
+                alt="womanpng"
+              />
+            </div>
+          </div>
+          <div className="greenCont w-[172px] h-[112px] rounded-2xl bg-[var(--color-trumpetCont)] ">
+            <div className="relative">
+              <Image
+                className="pt-[80px] mx-auto"
+                src="/Audio-call.svg"
+                width={81}
+                height={14}
+                alt="womanpng"
+              />
+
+              <Image
+                className="absolute bottom-8 left-8"
+                src="/trumpet.svg"
+                width={91}
+                height={106}
                 alt="womanpng"
               />
             </div>
